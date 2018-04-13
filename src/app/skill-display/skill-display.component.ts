b756524d7db123ebd9d5253d641d88de6363c8d6
Giripriya <TUMKURGIRIPRIYA@GMAIL.com>
2018-04-13 06:07:56 +0530

    fontAwesome

--- src/app/skill-display/skill-display.component.ts
+++ src/app/skill-display/skill-display.component.ts
@@ -21,7 +21,7 @@ export class SkillDisplayComponent implements OnInit {
     }
   updateSkill(evt : Event)
     {
-        updateFlag();
+        this.updateFlag();
         console.log(evt);
     }
   updateFlag() 
